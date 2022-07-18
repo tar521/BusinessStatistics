d959efbfe3a93dfb32fdf93e1abd5555f917dab0
Tristram Reed <tar123@txstate.edu>
2022-07-18 14:03:14 -0500

    UserController simple apis

--- BusinessStatistics/src/main/java/com/cognixia/jump/controller/UserController.java
+++ BusinessStatistics/src/main/java/com/cognixia/jump/controller/UserController.java
@@ -1,7 +1,17 @@
 package com.cognixia.jump.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cognixia.jump.model.User;
+import com.cognixia.jump.repository.UserRepository;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -10,4 +20,24 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "user", description = "API for user actions")
 public class UserController {
 
+	@Autowired
+	UserRepository repo;
+	
+	@GetMapping("/user")
+	public List<User> getUsers() {
+		return repo.findAll();
+	}
+	
+	@GetMapping("/user/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable int id) {
+		Optional<User> found = repo.findById(null);
+		
+		if (found.isEmpty()) {
+			return ResponseEntity.status(404).body("User with id: " + id + " was not found.");
+		}
+		else {
+			return ResponseEntity.status(200).body(found.get());
+		}
+	}
+	
 }
