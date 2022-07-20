1a2a2c5f29cc6f2cb8e8518acb552553a1569338
Albey Kappil <50112186+albkappil@users.noreply.github.com>
2022-07-20 12:52:12 -0500

    Logout functionality

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState, React} from 'react';
 import './App.css'; 
 import {BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Dashboard from './Components/Dashboard/MainDashboard'; 
@@ -11,24 +11,33 @@ import DeptFetch from './Components/Dashboard/Models/DeptFetch.js';
 
 // done below
 function App() { 
-  const {token, setToken} = useToken();
 
+  const {token, setToken} = useToken();
+  function logout(){
+    sessionStorage.removeItem('token');
+  
+    window.location.href="/";
+  }
+  
   if(!token) {
+   
     return <Login setToken={setToken} />
   } 
- 
   
+  
+
   return (
     <div className="container">
-      
+      <button onClickCapture={logout}>logout user</button>
       <BrowserRouter>   
           <Routes>
             <Route path='/' element = {<Home />}>
+              <Route path='/Login' element = {<Login />}/>
               <Route path='/Dashboard' element = {<Dashboard />}/>
               <Route path='/DeptFetch' element = {<DeptFetch />}/>
               <Route path='/Preferences' element = {<Preferences/>}/>   
             </Route>
-            <Route path='/Login' element = {<Login setToken={setToken}/>}/>   
+             
           </Routes> 
       </BrowserRouter>
     </div>
