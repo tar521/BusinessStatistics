bd5c3acf3ed9c70bac03a5d07f15302a205ef3de
Dylan McCullor <dmccullor0@gmail.com>
2022-07-21 16:05:16 -0500

    adding chart components to dashboard

--- react-app/src/Components/Dashboard/MainDashboard.js
+++ react-app/src/Components/Dashboard/MainDashboard.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import SalesChart from '../../SalesChart';
+import GoalGauge from '../../GoalGauge';
+import PieChart from '../../PieChart';
+
+const id = 2;
+const dept_id = 1;
 
  const MainDashboard=(props) => {
   return(
-    <h2>Business Statistics Dashboard</h2>
+    <div>
+      <h2>Business Statistics Dashboard</h2>
+      <div className='container' id='saleschart'>
+        <SalesChart/>
+      </div>
+      <div className='parent'>
+        <div className='child' style={{width: '40%'}} align='left'>
+          <GoalGauge/>
+        </div>
+        <div className='child' style={{width: '40%'}} align='right'>
+          <PieChart/>
+        </div>
+      </div>
+    </div>
   );
 }
 export default MainDashboard
